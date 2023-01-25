02ddbf6a960cb275843d386e10288ec0ac3f75b3
Vitalii Mikhailov <personal@aragas.org>
2023-01-25 12:12:14 +0200

    Added param_bool

--- src/BUTR.NativeAOT.Shared/Common.Native.h
+++ src/BUTR.NativeAOT.Shared/Common.Native.h
@@ -36,6 +36,7 @@ namespace Common
 
         typedef char16_t param_string;
         typedef char16_t param_json;
+        typedef uint8_t param_bool;
 
         typedef struct return_value_void
         {
