1e92c2c6fccb1d188931c459990b31648b9d6585
Vitalii Mikhailov <personal@aragas.org>
2023-01-28 10:47:42 +0200

    Combined them back

--- src/BUTR.NativeAOT.Shared/Common.Native.h
+++ src/BUTR.NativeAOT.Shared/Common.Native.h
@@ -23,49 +23,131 @@
 #ifndef SRC_COMMON_BINDINGS_H_
 #define SRC_COMMON_BINDINGS_H_
 
-typedef unsigned short param_string;
-typedef unsigned short param_json;
-typedef unsigned char param_bool;
-typedef signed int param_int;
-typedef unsigned int param_uint;
-typedef void param_ptr;
+#ifdef __cplusplus
+#include <memory>
+#include <string>
+#include <cstdint>
+#include <cuchar>
 
-typedef struct return_value_void
+namespace Common
 {
-    param_string *error;
-} return_value_void;
-typedef struct return_value_string
-{
-    param_string *error;
-    param_string *value;
-} return_value_string;
-typedef struct return_value_json
-{
-    param_string *error;
-    param_json *value;
-} return_value_json;
-typedef struct return_value_bool
-{
-    param_string *error;
-    param_bool value;
-} return_value_bool;
-typedef struct return_value_int32
-{
-    param_string *error;
-    param_int value;
-} return_value_int32;
-typedef struct return_value_uint32
-{
-    param_string *error;
-    param_uint value;
-} return_value_uint32;
-typedef struct return_value_ptr
-{
-    param_string *error;
-    param_ptr *value;
-} return_value_ptr;
+    extern "C"
+    {
 
-return_value_ptr* __cdecl alloc(size_t size);
-return_value_void* __cdecl dealloc(const param_ptr* p_handler);
+#endif
+
+#ifdef __cplusplus
+        typedef char16_t param_string;
+        typedef char16_t param_json;
+        typedef uint8_t param_bool;
+        typedef int32_t param_int;
+        typedef uint32_t param_uint;
+        typedef void param_ptr;
+#else
+        typedef unsigned short param_string;
+        typedef unsigned short param_json;
+        typedef unsigned char param_bool;
+        typedef signed int param_int;
+        typedef unsigned int param_uint;
+        typedef void param_ptr; 
+#endif
+
+        typedef struct return_value_void
+        {
+            param_string *error;
+        } return_value_void;
+        typedef struct return_value_string
+        {
+            param_string *error;
+            param_string *value;
+        } return_value_string;
+        typedef struct return_value_json
+        {
+            param_string *error;
+            param_json *value;
+        } return_value_json;
+        typedef struct return_value_bool
+        {
+            param_string *error;
+            param_bool value;
+        } return_value_bool;
+        typedef struct return_value_int32
+        {
+            param_string *error;
+            param_int value;
+        } return_value_int32;
+        typedef struct return_value_uint32
+        {
+            param_string *error;
+            param_uint value;
+        } return_value_uint32;
+        typedef struct return_value_ptr
+        {
+            param_string *error;
+            param_ptr *value;
+        } return_value_ptr;
+
+        return_value_ptr* __cdecl alloc(size_t p_handler);
+        return_value_void* __cdecl dealloc(const param_ptr* p_handler);
+
+#ifdef __cplusplus
+    }
+
+    template <typename T>
+    struct deleter
+    {
+        void operator()(const T *ptr) const { dealloc(ptr); }
+    };
+
+    using del_void = std::unique_ptr<return_value_void, deleter<return_value_void>>;
+    using del_string = std::unique_ptr<return_value_string, deleter<return_value_string>>;
+    using del_json = std::unique_ptr<return_value_json, deleter<return_value_json>>;
+    using del_bool = std::unique_ptr<return_value_bool, deleter<return_value_bool>>;
+    using del_int32 = std::unique_ptr<return_value_int32, deleter<return_value_int32>>;
+    using del_uint32 = std::unique_ptr<return_value_uint32, deleter<return_value_uint32>>;
+    using del_ptr = std::unique_ptr<return_value_ptr, deleter<return_value_ptr>>;
+
+    char16_t *Copy(const std::u16string str)
+    {
+        const auto src = str.c_str();
+        const auto srcChar16Length = str.length();
+        const auto srcByteLength = srcChar16Length * sizeof(char16_t);
+        const auto size = srcByteLength + sizeof(char16_t);
+
+        auto dst = (char16_t *)malloc(size);
+        if (dst == nullptr)
+        {
+            throw std::bad_alloc();
+        }
+        std::memmove(dst, src, srcByteLength);
+        dst[srcChar16Length] = '\0';
+        return dst;
+    }
+
+    std::unique_ptr<char16_t[], deleter<char16_t>> CopyWithFree(const std::u16string str)
+    {
+        return std::unique_ptr<char16_t[], deleter<char16_t>>(Copy(str));
+    }
+
+    const char16_t *NoCopy(const std::u16string str) noexcept
+    {
+        return str.c_str();
+    }
+
+    template <typename T>
+    T *Create(const T val)
+    {
+        const auto size = (size_t)sizeof(T);
+        auto dst = (T *)malloc(size);
+        if (dst == nullptr)
+        {
+            throw std::bad_alloc();
+        }
+        std::memcpy(dst, &val, sizeof(T));
+        return dst;
+    }
+
+}
+#endif
 
 #endif
