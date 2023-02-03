2cd7ceda351417000ba01e1feddf0ba97c190182
Vitalii Mikhailov <personal@aragas.org>
2023-02-03 13:03:44 +0200

    Fix

--- src/BUTR.NativeAOT.Analyzer/Analyzers/UnnecessaryAnalyzer.cs
+++ src/BUTR.NativeAOT.Analyzer/Analyzers/UnnecessaryAnalyzer.cs
@@ -14,14 +14,11 @@ using System.Linq;
 namespace BUTR.NativeAOT.Analyzer.Analyzers
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class AttributeAnalyzer : DiagnosticAnalyzer
+    public class UnnecessaryAnalyzer : DiagnosticAnalyzer
     {
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(
             RuleIdentifiers.UnnecessaryIsConstRule,
-            RuleIdentifiers.UnnecessaryIsPtrConstRule,
-            RuleIdentifiers.RequiredIsConstRule,
-            RuleIdentifiers.RequiredIsPtrConstRule,
-            RuleIdentifiers.RequiredConstMetaRule
+            RuleIdentifiers.UnnecessaryIsPtrConstRule
         );
 
         public override void Initialize(AnalysisContext context)
@@ -46,10 +43,6 @@ namespace BUTR.NativeAOT.Analyzer.Analyzers
             {
                 CheckReturnType(context, methodSymbol, methodConstMetadata);
             }
-            else
-            {
-                CheckReturnTypeNotExist(context, methodSymbol, methodDeclarationSyntax);
-            }
 
             for (var i = 0; i < methodDeclarationSyntax.ParameterList.Parameters.Count; i++)
             {
@@ -58,17 +51,13 @@ namespace BUTR.NativeAOT.Analyzer.Analyzers
 
                 if (parameterSymbol.Type is IFunctionPointerTypeSymbol functionPointerTypeSymbol)
                 {
-                    CheckFunctionPointerParameter(context, methodSymbol, parameterSymbol, functionPointerTypeSymbol, parameterSyntax.Type as FunctionPointerTypeSyntax);
+                    CheckFunctionPointerParameter(context, methodSymbol, parameterSymbol, functionPointerTypeSymbol);
                 }
                 else
                 {
                     if (Helper.TryGetParameterMetadata(methodSymbol, parameterSymbol, out var parameterConstMetadata))
                     {
                         CheckParameter(context, parameterSymbol, parameterConstMetadata);
-                    }
-                    else
-                    {
-                        CheckParameterNotExist(context, parameterSymbol, parameterSyntax);
                     }
                 }
             }
@@ -90,19 +79,6 @@ namespace BUTR.NativeAOT.Analyzer.Analyzers
                 context.ReportDiagnostic(RuleIdentifiers.ReportUnnecessaryIsConst(ctx, NameFormatter.ReflectionName(methodSymbol.ReturnType)));
             }
         }
-        private static void CheckReturnTypeNotExist(SyntaxNodeAnalysisContext context, IMethodSymbol methodSymbol, MethodDeclarationSyntax methodDeclarationSyntax)
-        {
-            if (methodSymbol.ReturnType is IPointerTypeSymbol)
-            {
-                var ctx = new GenericContext(context.Compilation, () => methodDeclarationSyntax.ReturnType.GetLocation(), context.ReportDiagnostic);
-                context.ReportDiagnostic(RuleIdentifiers.ReportRequiredIsPtrConstRule(ctx, NameFormatter.ReflectionName(methodSymbol.ReturnType)));
-            }
-            if (methodSymbol.ReturnType is IPointerTypeSymbol)
-            {
-                var ctx = new GenericContext(context.Compilation, () => methodDeclarationSyntax.ReturnType.GetLocation(), context.ReportDiagnostic);
-                context.ReportDiagnostic(RuleIdentifiers.ReportRequiredIsConstRule(ctx, NameFormatter.ReflectionName(methodSymbol.ReturnType)));
-            }
-        }
 
         private static void CheckParameter(SyntaxNodeAnalysisContext context, IParameterSymbol parameterSymbol, ConstMetadata constMetadata)
         {
@@ -120,31 +96,11 @@ namespace BUTR.NativeAOT.Analyzer.Analyzers
                 context.ReportDiagnostic(RuleIdentifiers.ReportRequiredIsPtrConstRule(ctx, NameFormatter.ReflectionName(parameterSymbol.Type)));
             }
         }
-        private static void CheckParameterNotExist(SyntaxNodeAnalysisContext context, IParameterSymbol parameterSymbol, ParameterSyntax parameterSyntax)
-        {
-            if (parameterSymbol.Type is IPointerTypeSymbol)
-            {
-                var ctx = new GenericContext(context.Compilation, () => parameterSyntax.GetLocation(), context.ReportDiagnostic);
-                context.ReportDiagnostic(RuleIdentifiers.ReportUnnecessaryIsPtrConst(ctx, NameFormatter.ReflectionName(parameterSymbol.Type)));
-            }
-            if (parameterSymbol.Type is IPointerTypeSymbol)
-            {
-                var ctx = new GenericContext(context.Compilation, () => parameterSyntax.GetLocation(), context.ReportDiagnostic);
-                context.ReportDiagnostic(RuleIdentifiers.ReportRequiredIsConstRule(ctx, NameFormatter.ReflectionName(parameterSymbol.Type)));
-            }
-        }
 
-        private static void CheckFunctionPointerParameter(SyntaxNodeAnalysisContext context, IMethodSymbol methodSymbol, IParameterSymbol parameterSymbol, IFunctionPointerTypeSymbol functionPointerTypeSymbol, FunctionPointerTypeSyntax functionPointerTypeSyntax)
+        private static void CheckFunctionPointerParameter(SyntaxNodeAnalysisContext context, IMethodSymbol methodSymbol, IParameterSymbol parameterSymbol, IFunctionPointerTypeSymbol functionPointerTypeSymbol)
         {
             var functionalPointerParameterReturnMetadata = Helper.TryGetFunctionalPointerParameterMetadata(parameterSymbol, functionPointerTypeSymbol, methodSymbol.Parameters.Length, out var val) ? val : ConstMetadata.Empty;
             var functionalPointerParameterParameters = methodSymbol.Parameters.Select((x, i) => Helper.TryGetFunctionalPointerParameterMetadata(x, functionPointerTypeSymbol, i, out var val) ? val : ConstMetadata.Empty).ToImmutableArray();
-
-            if (functionalPointerParameterReturnMetadata.AttributeData is null)
-            {
-                var ctx = new GenericContext(context.Compilation, () => functionPointerTypeSyntax.GetLocation(), context.ReportDiagnostic);
-                context.ReportDiagnostic(RuleIdentifiers.ReportRequiredConstMetaRule(ctx, NameFormatter.ReflectionName(functionPointerTypeSymbol)));
-                return;
-            }
 
             if (functionPointerTypeSymbol.Signature.ReturnType is not IPointerTypeSymbol && functionalPointerParameterReturnMetadata.IsPointingToConst)
             {
@@ -161,25 +117,13 @@ namespace BUTR.NativeAOT.Analyzer.Analyzers
                 var ctx = new GenericContext(context.Compilation, () => nodeRoot.GetLocation(), context.ReportDiagnostic);
                 context.ReportDiagnostic(RuleIdentifiers.ReportUnnecessaryIsConst(ctx, NameFormatter.ReflectionName(functionPointerTypeSymbol.Signature.ReturnType)));
             }
-            if (functionPointerTypeSymbol.Signature.ReturnType is IPointerTypeSymbol && !functionalPointerParameterReturnMetadata.IsPointingToConst)
-            {
-                var ctx = new GenericContext(context.Compilation, () => functionPointerTypeSyntax.ParameterList.Parameters.Last().GetLocation(), context.ReportDiagnostic);
-                context.ReportDiagnostic(RuleIdentifiers.ReportRequiredIsPtrConstRule(ctx, NameFormatter.ReflectionName(functionPointerTypeSymbol.Signature.ReturnType)));
-            }
-            if (functionPointerTypeSymbol.Signature.ReturnType is IPointerTypeSymbol && !functionalPointerParameterReturnMetadata.IsConst)
-            {
-                var ctx = new GenericContext(context.Compilation, () => functionPointerTypeSyntax.ParameterList.Parameters.Last().GetLocation(), context.ReportDiagnostic);
-                context.ReportDiagnostic(RuleIdentifiers.ReportRequiredIsConstRule(ctx, NameFormatter.ReflectionName(functionPointerTypeSymbol.Signature.ReturnType)));
-            }
 
             for (var i = 0; i < functionalPointerParameterParameters.Length; i++)
             {
                 if (i >= functionPointerTypeSymbol.Signature.Parameters.Length) continue;
-                if (i >= functionPointerTypeSyntax.ParameterList.Parameters.Count) continue;
                 
                 var functionPointerParameterParameterMetadata = functionalPointerParameterParameters[i];
                 var functionPointerParameterSymbol = functionPointerTypeSymbol.Signature.Parameters[i];
-                var functionPointerParameterSyntax = functionPointerTypeSyntax.ParameterList.Parameters[i];
 
                 if (functionPointerParameterSymbol.Type is not IPointerTypeSymbol && functionPointerParameterParameterMetadata.IsPointingToConst)
                 {
@@ -189,25 +133,12 @@ namespace BUTR.NativeAOT.Analyzer.Analyzers
                     var ctx = new GenericContext(context.Compilation, () => nodePtr.GetLocation(), context.ReportDiagnostic);
                     context.ReportDiagnostic(RuleIdentifiers.ReportUnnecessaryIsPtrConst(ctx, NameFormatter.ReflectionName(functionPointerParameterSymbol.Type)));
                 }
-
                 if (functionPointerParameterSymbol.Type is not IPointerTypeSymbol && functionPointerParameterParameterMetadata.IsConst)
                 {
                     if (functionalPointerParameterReturnMetadata.AttributeData.ApplicationSyntaxReference!.GetSyntax() is not AttributeSyntax nodeRootRoot) continue;
                     if (nodeRootRoot.Name is not GenericNameSyntax nodeRootRootName || i >= nodeRootRootName.TypeArgumentList.Arguments.Count || nodeRootRootName.TypeArgumentList.Arguments[i] is not { } nodeRoot) continue;
                     var ctx = new GenericContext(context.Compilation, () => nodeRoot.GetLocation(), context.ReportDiagnostic);
                     context.ReportDiagnostic(RuleIdentifiers.ReportUnnecessaryIsConst(ctx, NameFormatter.ReflectionName(functionPointerParameterSymbol.Type)));
-                }
-
-                if (functionPointerParameterSymbol.Type is IPointerTypeSymbol && !functionPointerParameterParameterMetadata.IsPointingToConst)
-                {
-                    var ctx = new GenericContext(context.Compilation, () => functionPointerParameterSyntax.GetLocation(), context.ReportDiagnostic);
-                    context.ReportDiagnostic(RuleIdentifiers.ReportRequiredIsPtrConstRule(ctx, NameFormatter.ReflectionName(functionPointerParameterSymbol.Type)));
-                }
-
-                if (functionPointerParameterSymbol.Type is IPointerTypeSymbol && !functionPointerParameterParameterMetadata.IsConst)
-                {
-                    var ctx = new GenericContext(context.Compilation, () => functionPointerParameterSyntax.GetLocation(), context.ReportDiagnostic);
-                    context.ReportDiagnostic(RuleIdentifiers.ReportRequiredIsConstRule(ctx, NameFormatter.ReflectionName(functionPointerParameterSymbol.Type)));
                 }
             }
         }
--- test/BUTR.NativeAOT.Analyzer.Test/AttributeAnalyzerTest.cs
+++ test/BUTR.NativeAOT.Analyzer.Test/AttributeAnalyzerTest.cs
@@ -13,12 +13,12 @@ public class AttributeAnalyzerTest : BaseTest
 {
     private static ProjectBuilder CreateProjectBuilder() => new ProjectBuilder()
         .WithTargetFramework(TargetFramework.Net7_0)
-        .WithAnalyzer<AttributeAnalyzer>();
+        .WithAnalyzer<RequiredAnalyzer>();
 
     [TestMethod]
     public async Task Unnecessary_CodeFix_Test()
     {
-        await CreateProjectBuilder().WithSourceCode(@$"
+        await CreateProjectBuilder().WithAnalyzer<UnnecessaryAnalyzer>().WithSourceCode(@$"
 namespace BUTR.NativeAOT.Analyzer.Test
 {{
     using System.Runtime.InteropServices;
@@ -84,9 +84,9 @@ namespace BUTR.NativeAOT.Analyzer.Test
     }
 
     [TestMethod]
-    public async Task ReturnType_Test()
+    public async Task Unnecessary_ReturnType_Test()
     {
-        await CreateProjectBuilder().WithSourceCode(@$"
+        await CreateProjectBuilder().WithAnalyzer<UnnecessaryAnalyzer>().WithSourceCode(@$"
 namespace BUTR.NativeAOT.Analyzer.Test
 {{
     using System.Runtime.CompilerServices;
@@ -112,6 +112,43 @@ namespace BUTR.NativeAOT.Analyzer.Test
 
 
         [UnmanagedCallersOnly]
+        public static void* Method12() => null;
+        [UnmanagedCallersOnly]
+        public static void* Method22() => null;
+        [UnmanagedCallersOnly]
+        public static void* Method32() => null;
+    }}
+}}{CodeBase}").ValidateAsync();
+    }
+    [TestMethod]
+    public async Task Required_ReturnType_Test()
+    {
+        await CreateProjectBuilder().WithAnalyzer<RequiredAnalyzer>().WithSourceCode(@$"
+namespace BUTR.NativeAOT.Analyzer.Test
+{{
+    using System.Runtime.CompilerServices;
+    using System.Runtime.InteropServices;
+    using BUTR.NativeAOT.Shared;
+
+    unsafe class Test
+    {{
+        [UnmanagedCallersOnly, IsConst]
+        public static void* Method10() => null;
+        [UnmanagedCallersOnly, IsConst<IsPtrConst>]
+        public static void* Method20() => null;
+        [UnmanagedCallersOnly, IsNotConst<IsPtrConst>]
+        public static void* Method30() => null;
+
+
+        [UnmanagedCallersOnly, IsConst]
+        public static void Method11() {{ }}
+        [UnmanagedCallersOnly, IsConst<IsPtrConst>]
+        public static void Method21() {{ }}
+        [UnmanagedCallersOnly, IsNotConst<IsPtrConst>]
+        public static void Method31() {{ }}
+
+
+        [UnmanagedCallersOnly]
         public static [||][||]void* Method12() => null;
         [UnmanagedCallersOnly]
         public static [||][||]void* Method22() => null;
@@ -122,9 +159,9 @@ namespace BUTR.NativeAOT.Analyzer.Test
     }
 
     [TestMethod]
-    public async Task Parameter_Test()
+    public async Task Unnecessary_Parameter_Test()
     {
-        await CreateProjectBuilder().WithSourceCode(@$"
+        await CreateProjectBuilder().WithAnalyzer<UnnecessaryAnalyzer>().WithSourceCode(@$"
 namespace BUTR.NativeAOT.Analyzer.Test
 {{
     using System.Runtime.CompilerServices;
@@ -154,11 +191,44 @@ namespace BUTR.NativeAOT.Analyzer.Test
     }}
 }}{CodeBase}").ValidateAsync();
     }
+    [TestMethod]
+    public async Task Required_Parameter_Test()
+    {
+        await CreateProjectBuilder().WithAnalyzer<RequiredAnalyzer>().WithSourceCode(@$"
+namespace BUTR.NativeAOT.Analyzer.Test
+{{
+    using System.Runtime.CompilerServices;
+    using System.Runtime.InteropServices;
+    using BUTR.NativeAOT.Shared;
+
+    unsafe class Test
+    {{
+        [UnmanagedCallersOnly]
+        public static void Method10([IsConst] char* p) {{ }}
+
+        [UnmanagedCallersOnly]
+        public static void Method20([IsConst<IsPtrConst>] char* p) {{ }}
+
+        [UnmanagedCallersOnly]
+        public static void Method30([IsNotConst<IsPtrConst>] char* p) {{ }}
+
+
+        [UnmanagedCallersOnly]
+        public static void Method10([IsConst] char p) {{ }}
+
+        [UnmanagedCallersOnly]
+        public static void Method20([IsConst<IsPtrConst>] char p) {{ }}
+
+        [UnmanagedCallersOnly]
+        public static void Method30([IsNotConst<IsPtrConst>] char p) {{ }}
+    }}
+}}{CodeBase}").ValidateAsync();
+    }
 
     [TestMethod]
-    public async Task FunctionalParameter_Test()
+    public async Task Unnecessary_FunctionalParameter_Test()
     {
-        await CreateProjectBuilder().WithSourceCode(@$"
+        await CreateProjectBuilder().WithAnalyzer<UnnecessaryAnalyzer>().WithSourceCode(@$"
 namespace BUTR.NativeAOT.Analyzer.Test
 {{
     using System.Runtime.InteropServices;
@@ -167,11 +237,11 @@ namespace BUTR.NativeAOT.Analyzer.Test
     unsafe class Test
     {{
         [UnmanagedCallersOnly]
-        public static void Method10([ConstMeta<IsConst, IsConst>] delegate* unmanaged[Cdecl]<[||]char*, [||]void*> p) {{ }}
+        public static void Method10([ConstMeta<IsConst, IsConst>] delegate* unmanaged[Cdecl]<char*, void*> p) {{ }}
         [UnmanagedCallersOnly]
         public static void Method20([ConstMeta<IsConst<IsPtrConst>, IsConst<IsPtrConst>>] delegate* unmanaged[Cdecl]<char*, void*> p) {{ }}
         [UnmanagedCallersOnly]
-        public static void Method30([ConstMeta<IsNotConst<IsPtrConst>, IsNotConst<IsPtrConst>>] delegate* unmanaged[Cdecl]<[||]char*, [||]void*> p) {{ }}
+        public static void Method30([ConstMeta<IsNotConst<IsPtrConst>, IsNotConst<IsPtrConst>>] delegate* unmanaged[Cdecl]<char*, void*> p) {{ }}
 
 
         [UnmanagedCallersOnly]
@@ -188,6 +258,42 @@ namespace BUTR.NativeAOT.Analyzer.Test
         public static void Method22([ConstMeta<IsConst<IsPtrConst>, [||]IsConst<[||]IsPtrConst>>] delegate* unmanaged[Cdecl]<char*, void> p) {{ }}
         [UnmanagedCallersOnly]
         public static void Method32([ConstMeta<IsConst<IsPtrConst>, IsNotConst<[||]IsPtrConst>>] delegate* unmanaged[Cdecl]<char*, void> p) {{ }}
+    }}
+}}{CodeBase}").ValidateAsync();
+    }
+    [TestMethod]
+    public async Task Required_FunctionalParameter_Test()
+    {
+        await CreateProjectBuilder().WithAnalyzer<RequiredAnalyzer>().WithSourceCode(@$"
+namespace BUTR.NativeAOT.Analyzer.Test
+{{
+    using System.Runtime.InteropServices;
+    using BUTR.NativeAOT.Shared;
+
+    unsafe class Test
+    {{
+        [UnmanagedCallersOnly]
+        public static void Method10([ConstMeta<IsConst, IsConst>] delegate* unmanaged[Cdecl]<[||]char*, [||]void*> p) {{ }}
+        [UnmanagedCallersOnly]
+        public static void Method20([ConstMeta<IsConst<IsPtrConst>, IsConst<IsPtrConst>>] delegate* unmanaged[Cdecl]<char*, void*> p) {{ }}
+        [UnmanagedCallersOnly]
+        public static void Method30([ConstMeta<IsNotConst<IsPtrConst>, IsNotConst<IsPtrConst>>] delegate* unmanaged[Cdecl]<[||]char*, [||]void*> p) {{ }}
+
+
+        [UnmanagedCallersOnly]
+        public static void Method11([ConstMeta<IsConst, IsConst<IsPtrConst>>] delegate* unmanaged[Cdecl]<char, void*> p) {{ }}
+        [UnmanagedCallersOnly]
+        public static void Method21([ConstMeta<IsConst<IsPtrConst>, IsConst<IsPtrConst>>] delegate* unmanaged[Cdecl]<char, void*> p) {{ }}
+        [UnmanagedCallersOnly]
+        public static void Method31([ConstMeta<IsNotConst<IsPtrConst>, IsConst<IsPtrConst>>] delegate* unmanaged[Cdecl]<char, void*> p) {{ }}
+
+
+        [UnmanagedCallersOnly]
+        public static void Method12([ConstMeta<IsConst<IsPtrConst>, IsConst>] delegate* unmanaged[Cdecl]<char*, void> p) {{ }}
+        [UnmanagedCallersOnly]
+        public static void Method22([ConstMeta<IsConst<IsPtrConst>, IsConst<IsPtrConst>>] delegate* unmanaged[Cdecl]<char*, void> p) {{ }}
+        [UnmanagedCallersOnly]
+        public static void Method32([ConstMeta<IsConst<IsPtrConst>, IsNotConst<IsPtrConst>>] delegate* unmanaged[Cdecl]<char*, void> p) {{ }}
     }}
 }}{CodeBase}").ValidateAsync();
     }
